179bdc7de1efbc6e71c90b944d4ba142aa6c58b2
edson.camargo <dinhocmenezes@hotmail.com>
2018-08-08 09:22:23 -0300

    Using api the movie db

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -21,6 +21,9 @@ export class FeedPage {
   public contLikes: number = 0;
   public currentDate: any;
 
+  public listMovies = new Array<any>();
+  public urlMovie = "https://image.tmdb.org/t/p/w500/";
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -36,7 +39,7 @@ export class FeedPage {
     this.mp.getPopularMovies().subscribe(data => { // getLatestMovies é do tipo observable, ou seja, ele observa um retorno, então ele não vai passar sem ter uma resposta.
       const response = (data as any); // O response (data no console log), não tem o _body, mas se fizermos um type cast de any, ele vai ter.
       const resolve = JSON.parse(response._body); // 'JSON.parse', vai pegar esse valor (que será texto) e transformar em um objeto JSON.
-      console.log(resolve);
+      this.listMovies = resolve.results; // O atributo 'results' é a lista de filmes.
     }, error => {
       console.log(error);
     })
